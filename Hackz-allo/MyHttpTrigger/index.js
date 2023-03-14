33cdddc17c169cc40de380c2e137dc947ad38fb1
009kin <2011140009m@ed.fuk.kindai.ac.jp>
2023-03-14 20:21:16 +0900

    html connect

--- Hackz-allo/MyHttpTrigger/index.js
+++ Hackz-allo/MyHttpTrigger/index.js
@@ -1,13 +1,24 @@
 module.exports = async function (context, req) {
+    const fs = require('fs').promises;
     context.log('JavaScript HTTP trigger function processed a request.');
+    context.log(__dirname);
+    let status;
+    let body;
+    const content_type = 'text/html';
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    try {
+        const text = await fs.readFile(__dirname + '/main.html', 'utf-8');
+        status = 200;
+        body = text;
+    } catch (e) {
+        status = 500;
+        body = `Internal Server Error: ${e}`;
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: body,
+        status: status,
+        headers: { 'Content-Type': 'text/html' }
     };
 }
